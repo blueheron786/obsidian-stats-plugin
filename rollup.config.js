08c19f2d416e1a96df559ab1efea33030394aa80
blueheron786 <blueheron786@gmail.com>
2025-06-19 22:21:24 -0400

    Paperwork

---
+++ rollup.config.js
@@ -0,0 +1,12 @@
+import typescript from 'rollup-plugin-typescript2';
+
+export default {
+  input: 'main.ts',
+  output: {
+    file: 'main.js',
+    format: 'cjs'
+  },
+  plugins: [
+    typescript()
+  ]
+};
